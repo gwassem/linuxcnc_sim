c4895b6ceb20d42309a37610d5724f2e5f5b6419
Your Name <146386245+gwassem@users.noreply.github.com>
2025-10-20 17:29:48 -0300

    fixing run_linuxcnc_instances.sh

--- run_linuxcnc_instances.sh
+++ run_linuxcnc_instances.sh
@@ -6,25 +6,39 @@ if ! docker images --format '{{.Repository}}' | grep -q "^${IMAGE_NAME}$"; then
   docker build -t ${IMAGE_NAME} .
 fi
 
-# Número de instâncias (ajuste conforme necessidade)
-INSTANCES=1
+#read -p "Quantas instâncias deseja abrir? " INSTANCES
+#read -p "Quer abrir os terminais? " TERMINALS
 
-for i in $(seq 1 $INSTANCES); do
+
+for i in $(seq 1 1); do #$(seq 1 $INSTANCES); do
   NAME="cnc_${i}"
   CONFIG_DIR="$HOME/linuxcnc-configs/${NAME}"
 
-  echo "[INFO] Preparando diretório de configuração para $NAME..."
   mkdir -p "$CONFIG_DIR"
-
-  echo "[INFO] Abrindo instância $i ($NAME)..."
-  docker run -it --rm --privileged \
-    --ipc=host \
-    --name ${NAME} \
-    -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v "$CONFIG_DIR":/home/linuxcnc/linuxcnc/configs \
-    ${IMAGE_NAME}
+#  if [$TERMINALS -eq s]; then
+#     docker run -it --rm --privileged \
+#     --ipc=host \
+#     --name cnc_${i} \
+#     -e DISPLAY=$DISPLAY \
+#     -v /tmp/.X11-unix:/tmp/.X11-unix \
+#     -v /home/linuxcnc-configs/cnc_${i}:/home/configs \
+#     --entrypoint /bin/bash \
+#     linuxcnc-2.9  &&\
+#     exec x-terminal-emulator 
+#  else
+    echo "[INFO] Abrindo instância $i ($NAME)..."
+     docker run -it --rm --privileged \
+     --ipc=host \
+     --name linuxcnc_instance_1 \
+     -e DISPLAY=$DISPLAY \
+     -v /tmp/.X11-unix:/tmp/.X11-unix \
+     -v /home/linuxcnc-configs/linuxcnc_instance_1:/home/linuxcnc/configs \
+     --entrypoint /bin/bash \
+     linuxcnc-2.9
+#  fi
 done
 
 echo "[INFO] Todas as instâncias foram iniciadas. Use 'docker ps' para verificar."
+
+
 
